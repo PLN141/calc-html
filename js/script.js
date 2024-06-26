24828a2661b70486d890274c0ceed59ea66a8acb
PLN141 <pedrolaurindoneto>
2024-06-25 21:42:28 -0300

    subindo a funcao mostrar resultado

--- js/script.js
+++ js/script.js
@@ -1,23 +1,44 @@
 const n1 = document.getElementById('numero1');
 const n2 = document.getElementById('numero2');
+const resultado = document.getElementById('resultado');
 
 function soma() {
-    console.log('soma', parseFloat(n1.value)+parseFloat(n2.value));
+    console.log('soma', parseFloat(n1.value) + parseFloat(n2.value));
+
+    const valorCalculo = parseFloat(n1.value) + parseFloat(n2.value)
+
+    resultado.innerText = valorCalculo
 }
 //soma();
 
-function diminuir(){
-    console.log('diminuir', parseFloat(n1.value)-parseFloat(n2.value));
+function diminuir() {
+    console.log('diminuir', parseFloat(n1.value) - parseFloat(n2.value));
+
+    const valorCalculo = parseFloat(n1.value) - parseFloat(n2.value)
+
+    resultado.innerText = valorCalculo
 }
 
-function multiplicar(){
-console.log('multiplicar', parseFloat(n1.value)*parseFloat(n2.value));
+function multiplicar() {
+    console.log('multiplicar', parseFloat(n1.value) * parseFloat(n2.value));
+
+    const valorCalculo = parseFloat(n1.value) * parseFloat(n2.value)
+
+    resultado.innerText = valorCalculo
 }
 
-function dividir(){
-    if(parseFloat(n2.value) != 0){
-        console.log('dividir', parseFloat(n1.value)/parseFloat(n2.value));
-        } else {
-            alert('Não pode ser feito a divisão por zero')
-        }
+function dividir() {
+    if (parseFloat(n2.value) != 0) {
+        console.log('dividir', parseFloat(n1.value) / parseFloat(n2.value));
+    } else {
+        alert('Não pode ser feito a divisão por zero')
+    }
+
+    const valorCalculo = parseFloat(n1.value) / parseFloat(n2.value)
+
+    resultado.innerText = valorCalculo
+}
+
+function exibirResultado(valorCalculo) {
+    resultado.innerText = valorCalculo
 }
